34757226cee5cd11c56e37ce83581487c609d607
ksnghyk <hani103201@naver.com>
2023-04-25 15:10:23 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import { useState } from "react";
 
+
+
 function App() {
-  const useInput = (initialValue, validator) => {
-    const [value, setValue] = useState(initialValue);
-    const onChange = (event) => {
-      const value = event.target.value;
-      let willUpdate = true;
-      if(typeof validator === "function"){
-        willUpdate = validator(value);
-      }
-      if(willUpdate){
-        setValue(value);
-      }
-    }
-    return { value, onChange };
-  }
-  const maxLen = value => value.length < 10;
-  const name = useInput("Mr.", maxLen);
+
   return (
     <div>
-      <h1>useInput</h1>
-      <input placeholder="Name" {...name} />
+      <h1></h1>
+
     </div>
   );
 }
