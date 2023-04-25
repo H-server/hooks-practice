1052bc804c4d28a0d1fdd0bf4015ebbfa23f360c
ksnghyk <hani103201@naver.com>
2023-04-25 16:24:27 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
 
+  useEffect(() => {
+    document.title = title;
+  }, [title])
+
+  return [title, setTitle];
+}
 
 function App() {
+  const [title, setTitle] = useTitle("initialTitle");
+  const handleButtonClick =  () => {
+    setTitle("newTitle");
+  }
 
   return (
     <div>
-      <h1></h1>
-
+      <h1>useTitle</h1>
+      <h2>{title}</h2>
+      <button onClick={handleButtonClick}>title update</button>
     </div>
   );
 }
